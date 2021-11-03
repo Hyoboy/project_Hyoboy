bd714825c1f14c25ce2cf112f12520d30d15fd21
Hyoboy <gyejd4036@naver.com>
2021-11-03 22:59:17 +0900

    로그인 이벤트처리 추가, 계좌정보 추가

--- openApi/openApi.py
+++ openApi/openApi.py
@@ -14,20 +14,34 @@ class OpenApi(QAxWidget) :
         self.set_signal_slots()
         # 키움증권 로그인
         self.CommConnect()
+        # 키움증권 계좌정보
+        self.account_info()
+
+    def getLoginInfo(self, tag):
+        try:
+            rtn = self.dynamicCall("GetLoginInfo(QString)", tag)
+            return rtn
+        except Exception as e:
+            print(e)
+
+    def account_info(self):
+        account_number = self.getLoginInfo('ACCNO')
+        print(account_number)
+        self.account_number = account_number.split(';')[0]
+        print('계좌번호 : ' + self.account_number)
 
     def createKiwoomInstance(self) :
         # 키움증권 인스턴스 생성
-        self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
+        self.setControl('KHOPENAPI.KHOpenAPICtrl.1')
 
     def set_signal_slots(self) :
-        #try :
+        try :
             self.OnEventConnect.connect(self.eventConnect)
-        #except Exception as e :
-            #print(e)
+        except Exception as e :
+            print(e)
         
     # 로그인 이벤트처리
     def eventConnect(self, errCode) :
-        print(errCode)
         if errCode == 0 :
             print('로그인 성공')
         elif errCode == -100 :
@@ -36,20 +50,24 @@ class OpenApi(QAxWidget) :
         elif errCode == -101 :
             # 서버 연결 실패
             print('실패')
+        # 이벤트루프 종료
+        self.login_event_loop.exit()
 
     def CommConnect(self) :
-        # 로그인
-        self.dynamicCall("CommConnect()")
-        # 로그인 후 데이터처리 대기코드 (아래코드없이 실행하면 로그인처리 후 바로 코드가 실행되어 서비스 종료됨)
-        self.login_event_loop = QEventLoop()
-        # event loop 전처리
-        # 여기 지우고 사용
-        #
-        self.login_event_loop.exec_()
-        # event loop 후처리
-        # 여기 지우고 사용
-        #
-
+        try :
+            # 로그인
+            self.dynamicCall('CommConnect()')
+            # 로그인 후 데이터처리 대기코드 (아래코드없이 실행하면 로그인처리 후 바로 코드가 실행되어 서비스 종료됨)
+            self.login_event_loop = QEventLoop()
+            # event loop 전처리
+            # 여기 지우고 사용
+            #
+            self.login_event_loop.exec_()
+            # event loop 후처리
+            # 여기 지우고 사용
+            #
+        except Exception as e :
+            print(e)
 
     def checkEnv(self) :
         is_64bit = sys.maxsize > 2 ** 32
